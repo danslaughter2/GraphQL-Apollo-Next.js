143dabdc0b88d6447516b486de75cdefa120ac41
Omavi Walker <omavi@perfectsensedigital.com>
2021-09-10 17:48:38 -0400

    Updates autogenerated REST API files

--- generated/helloBrightspot.ts
+++ generated/helloBrightspot.ts
@@ -1,26 +1,13 @@
-import { HelloBrightspotQuery, HelloBrightspotQueryVariables, useHelloBrightspotQuery as originalQuery } from './graphql'
+import {
+    HelloBrightspotQuery, HelloBrightspotQueryVariables, useHelloBrightspotQuery as originalQuery
+} from './graphql'
 import * as Apollo from '@apollo/client'
-import { useEffect, useState } from 'react'
 import { ApolloQueryResult, QueryResult } from '@apollo/react-hooks'
+import { generateUseQuery } from '../rest/query'
 
+const useQuery = generateUseQuery('helloBrightspot', originalQuery)
 export const useHelloBrightspotQuery = (baseOptions?: Apollo.QueryHookOptions<HelloBrightspotQuery, HelloBrightspotQueryVariables>):
     ApolloQueryResult<HelloBrightspotQuery> | QueryResult<HelloBrightspotQuery, HelloBrightspotQueryVariables> => {
 
-    const [response, setResponse] = useState<ApolloQueryResult<HelloBrightspotQuery>>(originalQuery(baseOptions))
-
-    const searchParams = new URLSearchParams()
-
-    Object.entries(baseOptions.variables).forEach(([key, value]) => {
-        searchParams.append(key, value)
-    })
-
-    useEffect(() => {
-        if (!response.data) {
-            fetch(`${process.env.NEXT_PUBLIC_HOST}/api/helloBrightspot?${searchParams.toString()}`)
-            .then(response => response.json())
-            .then(d => setResponse(d))
-        }
-    }, [])
-
-    return response
+    return useQuery(baseOptions)
 }
--- pages/api/helloBrightspot.js
+++ pages/api/helloBrightspot.js
@@ -1,34 +1,6 @@
-import { gql, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
-import fetch from 'isomorphic-unfetch'
+import Handler from '../../rest/handler'
 
+const handler = new Handler('./components/HelloBrightspot/helloBrightspot.graphql', ['id', 'path'])
 export default async (req, res) => {
-
-  const { previewId } = req.query;
-  const variables = previewId != null ? { id: previewId } : {}
-  variables.id = req.query.id
-  variables.path = req.query.path
-
-  const query = gql`
-      query HelloBrightspot($id: ID, $path: String) {
-        HelloBrightspot(id: $id, path: $path) {
-          message
-        }
-      }
-  `
-
-  const client = new ApolloClient({
-    link: createHttpLink({
-        fetch,
-        uri: process.env.GRAPHQL_URL,
-        headers: {
-          'X-API-Key': process.env.API_KEY
-        }
-      }
-    ),
-    cache: new InMemoryCache()
-  })
-
-  const data = await client.query({ query, variables })
-
-  res.status(200).json(data)
+    await handler.handle(req, res)
 }
